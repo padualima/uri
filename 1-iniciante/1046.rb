e83bca4f7b152384693ceb05d038b89b9c456b3f
Pádua Lima <padua-lima@hotmail.com>
2019-04-16 03:12:13 -0300

    teste

--- 1-iniciante/1046.rb
+++ 1-iniciante/1046.rb
@@ -10,4 +10,3 @@ Apresente a duração do jogo conforme exemplo abaixo.
           16 2                      O JOGO DUROU 10 HORA(S)
           0 0                       O JOGO DUROU 24 HORA(S)
           2 16                      O JOGO DUROU 14 HORA(S)
-weex
