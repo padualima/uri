6e90f58b07e10e2c04a3cba0a37c99726d07f819
Pádua Lima <padua-lima@hotmail.com>
2019-04-19 09:39:48 -0300

    months

---
+++ 1-iniciante/1052.rb
@@ -0,0 +1,5 @@
+mes = gets.to_i
+meses = { 1 => "Janeiro", 2 => "Fevereiro", 3 => "Março", 4 => "April",
+          5 => "Maio", 6 => "Junio", 7 => "Julho", 8 => "Agosto", 9 => "Setembro",
+          10 => "Outubro", 11 => "November", 12 => "Dezember"}
+puts meses[mes]
