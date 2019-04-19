30dba8c8b9271e7369f770565bb9b4d855833e4a
Pádua Lima <padua-lima@hotmail.com>
2019-04-18 23:47:14 -0300

    DDD

---
+++ 1-iniciante/1050.rb
@@ -0,0 +1,22 @@
+ddd = gets.to_i
+
+case ddd
+when 11
+  puts "Sao Paulo"
+when 19
+  puts "Campinas"
+when 21
+  puts "Rio de Janeiro"
+when 27
+  puts "Vitoria"
+when 31
+  puts "Belo Horizonte"
+when 32
+  puts "Juiz de Fora"
+when 61
+  puts "Brasilia"
+when 71
+  puts "Salvador"
+else
+  puts "DDD nao cadastrado"
+end
